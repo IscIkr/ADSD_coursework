64cbbd0ff32d36282a6a47a53e7c3dc3c6e0c0db
Unknown <aurelien.dworniczek@gmail.com>
2018-02-14 22:13:09 +0000

    Cordic

---
+++ CORDIC/cmake-build-debug/CMakeFiles/CORDIC.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CORDIC.dir/cordic.cpp.obj"
+  "CMakeFiles/CORDIC.dir/cordic_prb.cpp.obj"
+  "CORDIC.pdb"
+  "CORDIC.exe"
+  "CORDIC.exe.manifest"
+  "libCORDIC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CORDIC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
