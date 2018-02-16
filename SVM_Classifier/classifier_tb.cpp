d5b034176e99b3768a9f5834c3736f92c94d44bc
Unknown <stavros1410@hotmail.com>
2018-02-16 19:44:24 +0000

    Include error rate
    Compare our output to golden output(testLabelData)

--- SVM_Classifier/classifier_tb.cpp
+++ SVM_Classifier/classifier_tb.cpp
@@ -12,7 +12,7 @@
 
 int main () {
 
-    int     i, j, flag=0;             //flag used to compare to the results
+    int     i, j, error=0;             //flag used to compare to the results
 //int     test_ones = 0, out_ones=0;
     double  x[N], sv[NSV][N];
     int     out[SAMPLES];
@@ -31,25 +31,18 @@ int main () {
 //for (j=0;j<N;j++) printf("%d: x[%d] = %f\n", i*N + j, j, x[j]); //debugging: check if x is ok
         out[i] = classifier(alpha, sv, x, bias);
 //fprintf(fpout,"%d\n",out[i]); //debugging: check output (1/0)
-        if (out[i] != testDataLabel[i]) flag++;
+        if (out[i] != testDataLabel[i]) error++;
 //if (testDataLabel[i] == 1) test_ones++;
 //if (out[i] == 1) out_ones++;
     }
 //printf("Given tanh:\n");
 //printf("CORDIC tanh:\n");
 //printf("#Test_Ones = %d. Percentage = %.5f\%\n\n", test_ones, 100*(double)test_ones/SAMPLES);
-//printf("#Out_Ones = %d. Percentage = %.5f\%\n\n", out_ones, 100*(double)out_ones/SAMPLES);
 
-    printf ("Comparing against output data \n");
-    if (flag!=0) {
-        fprintf(stdout, "*********************************************\n");
-        fprintf(stdout, "FAIL: Output DOES NOT match the golden output\n");
-        fprintf(stdout, "*********************************************\n");
-        return 1;
-    } else {
-        fprintf(stdout, "*******************************************\n");
-        fprintf(stdout, "PASS: The output matches the golden output!\n");
-        fprintf(stdout, "*******************************************\n");
-        return 0;
-    }
+    fprintf(stdout, "Comparing result against golden output\n");
+    fprintf(stdout, "**************************************\n");
+    fprintf(stdout, "The classification error rate is %.2f\%\n", 100*(double)error/SAMPLES);
+    fprintf(stdout, "**************************************\n");
+    return 0;
+
 }
